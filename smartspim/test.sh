d8358932581646e7c55973d75a43e7151819e369
Austin Hoag <austinthomashoag@gmail.com>
2020-12-07 14:56:42 -0500

    rewrite smartspim stitching pipeline to propagate errors to slurm

--- smartspim/test.sh
+++ smartspim/test.sh
@@ -6,15 +6,19 @@
 #SBATCH -o logs/smartspim_stitch_%j.out        # STDOUT #add _%a to see each array job
 #SBATCH -e logs/smartspim_stitch_%j.err        # STDERR #add _%a to see each array job
 
-# echo "In the directory: `pwd` "
-# echo "As the user: `whoami` "
-# echo "on host: `hostname` "
+echo "In the directory: `pwd` "
+echo "As the user: `whoami` "
+echo "on host: `hostname` "
+
+#specifications
+cat /proc/$$/status | grep Cpus_allowed_list
+cat /proc/meminfo
 
 module load anacondapy/5.3.1
 . activate lightsheet
 
-# echo "Experiment name / TeraStitcher folder hierarchy:" "$1"
-# echo "Storage directory:" "$2"
+echo "Experiment name / TeraStitcher folder hierarchy:" "$1"
+echo "Storage directory:" "$2"
 
 #import
 OUT0=$(sbatch ts_smartspim_import.sh "$1")
@@ -31,9 +35,3 @@ echo $OUT2
 #merge
 OUT3=$(sbatch --dependency=afterok:${OUT2##* } ts_smartspim_merge.sh "$1" "$2")
 echo $OUT3
-
-#functionality
-#go to smartspim_pipeline folder and type sbatch spim_stitch.sh [path to terstitcher folder hierarchy] [destination of stitched directory]
-
-
-
